abf79b69f38c45bbcb23299317eae988d737d237
naresh1406 <nareshgupta1406@gmail.com>
2020-04-11 11:03:49 +0700

    class variables and methods

---
+++ src/main/oops/ClassMethod.py
@@ -0,0 +1,19 @@
+class Person:
+    number_of_people = 0
+
+    def __init__(self, name):
+        self.name = name
+        Person.add_person()
+
+    @classmethod
+    def number_of_people_(cls):
+        return cls.number_of_people
+
+    @classmethod
+    def add_person(cls):
+        cls.number_of_people += 1
+
+
+p1 = Person("tim")
+p2 = Person("jill")
+print(Person.number_of_people_())
