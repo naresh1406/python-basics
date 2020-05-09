c0ff199122d02b5e362a83bff5c7ea415afcbd84
naresh1406 <nareshgupta1406@gmail.com>
2020-05-09 20:50:23 +0700

    oops TryExcept

--- src/main/oops/TryExcept.py
+++ src/main/oops/TryExcept.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class TryExcept:  # class name
 
     def raise_exception(self, name):  # method definition
@@ -13,6 +16,38 @@ def main():  # main function to initialize instance and call methods
         print("exception raised")
 
     print(name)
+
+    random_list = ['a', 0, '2']
+    for entry in random_list:
+        try:
+            print("The entry is " + entry)
+            r = 1 / int(entry)
+            break
+        except:
+            # sys.exc_info()[0] - to print the type of exception
+            print("Oops! ", sys.exc_info()[0], "occurred.")
+            print("Next entry")
+            print()
+
+    print("The reciprocal of ", entry, "is ", r)
+
+    # catching specific exception
+    try:
+        # do something
+        pass
+
+    except ValueError:
+        # handle ValueError exception
+        pass
+
+    except (TypeError, ZeroDivisionError):
+        # handle multiple exceptions
+        # TypeError and ZeroDivisionError
+        pass
+
+    except:
+        # handle all other exceptions
+        pass
 
 
 if __name__ == '__main__':  # to run the main function
